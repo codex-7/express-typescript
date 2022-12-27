9128e04e2e1a587244734e5368beee93220dec71
0xMahabub <mahabub212002072@gmail.com>
2022-12-27 23:02:33 +0600

    fix:husky+docs

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -2,6 +2,13 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // env variable parseing
 
+/**----------------------------------------------------*
+ * @Environment variable setup guide
+ * export const env_name: type = value
+ * value can be accesed by `process.env.[key]`
+ * Where `key` will be mapped from your environment file
+ *-----------------------------------------------------*/
+
 export const port: number = Number(process.env.PORT) || 4000;
 export const databaseUrl: string = process.env.DB_URL || '';
 export const mode: string = process.env.NODE_ENV || 'development';
